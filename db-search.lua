f67ae8896599ec776e12fd6e96a005783e2c9381
Alexander Maribo <alexander.m.mackenzie@gmail.com>
2025-02-25 18:59:01 -0600

    Add Warbound for Old Expansions
    Also add Raid Consumables. Update iLvl breakpoint for low iLvl gear to be 490.

--- db-search.lua
+++ db-search.lua
@@ -2,24 +2,24 @@ local _, addon = ...
 
 local db = {
 	["Weapons"] = {
-		queryString = "(type = weapon or slot %= 'off hand' or slot %= 'held in') and not category = 'gear:' and ilvl > 300", 
+		queryString = "(type = weapon or slot %= 'off hand' or slot %= 'held in') and not category = 'gear:' and ilvl > 490", 
 		prio = 55, 
 	}, 
 	["Armour"] = {
-		queryString = "type = armor and not (slot %= 'off hand' or slot %= 'held in') and not category = 'gear:' and ilvl > 300", 
+		queryString = "type = armor and not (slot %= 'off hand' or slot %= 'held in') and not category = 'gear:' and ilvl > 490", 
 		prio = 54, 
 	}, 
 	["Jewellery"] = {
-		queryString = "slot = finger or slot = neck and not (category = 'gear:') and ilvl > 300", 
+		queryString = "slot = finger or slot = neck and not (category = 'gear:') and ilvl > 490", 
 		prio = 53, 
 	}, 
 	["Trinkets"] = {
-		queryString = "slot = trinket and not (category = 'gear:') and ilvl > 300", 
+		queryString = "slot = trinket and not (category = 'gear:') and ilvl > 490", 
 		prio = 52, 
 	}, 
 	["Low iLvl Gear"] = {
-		queryString = "(type = weapon or type = armor) and not (category = 'gear:') and ilvl <= 300", 
-		prio = 100, 
+		queryString = "(type = weapon or type = armor) and not (category = 'gear:') and ilvl <= 490", 
+		prio = 99, 
 	}, 
 	["WuE"] = {
 		queryString = "binding = wue", 
@@ -35,6 +35,14 @@ local db = {
 	}, 
 	["Warbound Other"] = {
 		queryString = "binding = warbound and (type != weapon and type != armor)", 
+		prio = 51, 
+	}, 
+	["Warbound - Old Expansions"] = {
+		queryString = "(wue or warbound) and expansion != TWW and ilvl <= 490", 
+		prio = 46, 
+	}, 
+	["Raid Consumables"] = {
+		queryString = "subtype = potion or subtype = flask or (type = consumable and rune) or name %= 'eggs' or name %= 'revenge' or name %= \"beledar's b\" and rarity >= common or (type = consumable and ironclaw) or (type = consumable and oil and exp = tww", 
 		prio = 50, 
 	}, 
 }
